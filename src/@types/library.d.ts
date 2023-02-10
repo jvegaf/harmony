90e26e01b610f15d99368d95e5ffc953e162d321
Jose Vega <josevega234@gmail.com>
2023-02-10 22:21:08 +0100

    clean
    Signed-off-by: Jose Vega <josevega234@gmail.com>

--- src/@types/library.d.ts
+++
@@ -1,7 +0,0 @@
-import { Track } from '../../electron/types/emusik';
-
-export type LibraryContextType = {
-  tracksCollection: Track[];
-  addTracks: (tracks: Track[]) => void;
-  updateTrack: (track: Track) => void;
-};
