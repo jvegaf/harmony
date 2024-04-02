7aebd631bf5a2e1abb5a07df3857d02dc1e1bc4a
Jose Vega <josevega234@gmail.com>
2024-04-02 02:02:51 +0200

    detail image

--- src/renderer/src/views/DetailView.tsx
+++ src/renderer/src/views/DetailView.tsx
@@ -3,13 +3,17 @@ import useAppStore from '@renderer/stores/useAppStore';
 import { FC, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useForm } from '@mantine/form';
-import { TextInput, Button } from '@mantine/core';
+import { TextInput, Button, Image } from '@mantine/core';
+import PlaceHolder from '../assets/placeholder.png';
 import classes from './DetailView.module.css';
+import { Track } from '@preload/emusik';
 
 export const DetailView: FC = () => {
   const contentHeight = useAppStore(state => state.contentHeight);
   const { trackId } = useParams();
   const getTrackFromId = useLibraryStore(state => state.getTrackFromId);
+  const getArtImage = useAppStore(state => state.getArtImage);
+  const [artSrc, setArtSrc] = useState<string | null>(null);
 
   const form = useForm({
     initialValues: {
@@ -23,12 +27,27 @@ export const DetailView: FC = () => {
     },
   });
 
+  const setArtwork = async (track: Track) => {
+    const artImage = await getArtImage(track);
+    if (artImage === null) {
+      setArtSrc(null);
+      return;
+    }
+    console.log(artImage.mime);
+    console.log(artImage.imageBuffer.length);
+    const blob = new Blob([artImage.imageBuffer], { type: artImage.mime });
+
+    const src = URL.createObjectURL(blob);
+    setArtSrc(src);
+  };
+
   useEffect(() => {
     console.log(trackId);
     const track = getTrackFromId(trackId);
     if (!track) return;
     form.setInitialValues(track);
     form.setValues(track);
+    setArtwork(track);
 
     return () => {
       form.reset();
@@ -36,12 +55,20 @@ export const DetailView: FC = () => {
   }, [trackId]);
 
   return (
-    <form onSubmit={form.onSubmit(values => console.log(values))}>
-      <div
-        className={classes.detailRoot}
-        style={{ height: contentHeight }}
-      >
-        <div className={classes.detailForm}>
+    <div
+      className={classes.detailRoot}
+      style={{ height: contentHeight }}
+    >
+      <div className={classes.detailImage}>
+        <Image
+          src={artSrc}
+          radius='lg'
+          h={250}
+          fallbackSrc={PlaceHolder}
+        />
+      </div>
+      <div className={classes.detailForm}>
+        <form onSubmit={form.onSubmit(values => console.log(values))}>
           <div className={classes.fullRow}>
             <TextInput
               label='Title'
@@ -94,8 +121,8 @@ export const DetailView: FC = () => {
             </Button>
             <Button type='submit'>Save</Button>
           </div>
-        </div>
+        </form>
       </div>
-    </form>
+    </div>
   );
 };
