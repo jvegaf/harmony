e387d7da8b09472f3dd294f67eb1259e06be2abe
Jose Vega <josevega234@gmail.com>
2024-12-22 19:48:31 +0100

    fixes

--- src/renderer/src/lib/utils-events.ts
+++ src/renderer/src/lib/utils-events.ts
@@ -1,5 +1,3 @@
-import { platform } from '@electron-toolkit/utils';
-
 export const isLeftClick = (e: React.MouseEvent): boolean => e.button === 0;
 export const isRightClick = (e: React.MouseEvent): boolean => e.button === 2;
 
@@ -23,9 +21,9 @@ export function preventNativeDefault(e: Event) {
  * - the cmd key is pressed on macOS
  */
 export function isCtrlKey(e: React.KeyboardEvent | React.MouseEvent | KeyboardEvent): boolean {
-  return (platform.isMacOS && e.metaKey) || (!platform.isMacOS && e.ctrlKey);
+  return e.ctrlKey;
 }
 
 export function isAltKey(e: React.KeyboardEvent | React.MouseEvent | KeyboardEvent): boolean {
-  return (platform.isMacOS && e.ctrlKey) || (!platform.isMacOS && e.metaKey);
+  return e.metaKey;
 }
