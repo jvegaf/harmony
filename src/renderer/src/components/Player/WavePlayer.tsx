01f8a610ce95a379b1fc94e63c201ba086a40cc2
Jose Vega <josevega234@gmail.com>
2025-04-07 21:30:38 +0200

    feat(player): replace @wavesurfer/react with custom hook
    - Removed @wavesurfer/react dependency from package.json and yarn.lock.
    - Added a custom useWavesurfer hook for better control and customization.
    - Updated WavePlayer component to use the new hook and refactored styles.
    - Replaced WavePlayer.module.css with WavePlayer.css for improved styling.

--- src/renderer/src/components/Player/WavePlayer.tsx
+++ src/renderer/src/components/Player/WavePlayer.tsx
@@ -1,28 +1,87 @@
-import { useWavesurfer } from '@wavesurfer/react';
-import { useRef, useEffect, useState } from 'react';
-import styles from './WavePlayer.module.css';
+import { useWavesurfer } from '../../hooks/useWavesurfer';
+import { useRef, useEffect, useState, RefObject, useMemo, useCallback } from 'react';
 import usePlayerStore, { usePlayerAPI } from '../../stores/usePlayerStore';
 import { PlayerStatus } from '../../../../preload/types/harmony';
+import WaveSurfer, { WaveSurferOptions } from 'wavesurfer.js';
+import './WavePlayer.css';
+
+const formatTime = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60);
+  const secondsRemainder = Math.round(seconds) % 60;
+  const paddedSeconds = `0${secondsRemainder}`.slice(-2);
+  return `${minutes}:${paddedSeconds}`;
+};
 
 function WavePlayer() {
-  const containerRef = useRef(null);
+  const containerRef = useRef<HTMLInputElement>(null);
+  const timeRef = useRef<HTMLInputElement>(null);
+  const durationRef = useRef<HTMLInputElement>(null);
+  const hoverRef = useRef<HTMLInputElement>(null);
   const playingTrack = usePlayerStore.use.playingTrack();
   const playerStatus = usePlayerStore.use.playerStatus();
   const playerAPI = usePlayerAPI();
   const [audioUrl, setAudioUrl] = useState('');
+  const [durationTrack, setDurationTrack] = useState<number>(0);
+  const [time, setTime] = useState<string>('0:00');
+  const [duration, setDuration] = useState<string>('0:00');
 
-  const { wavesurfer } = useWavesurfer({
-    container: containerRef,
-    autoplay: true,
-    height: 70,
-    waveColor: '#656566',
-    progressColor: '#eb4926',
-    cursorColor: 'transparent',
-    autoCenter: true,
-    barWidth: 2,
-    barGap: 2,
-    url: audioUrl,
-  });
+  const optionsMemo = useMemo((): Omit<WaveSurferOptions, 'container'> => {
+    let gradient, progressGradient;
+    if (typeof window !== 'undefined') {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d')!;
+
+      gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
+      gradient.addColorStop(0, '#656666');
+      gradient.addColorStop((canvas.height * 0.7) / canvas.height, '#656666');
+      gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff');
+      gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff');
+      gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#B1B1B1');
+      gradient.addColorStop(1, '#B1B1B1');
+
+      progressGradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
+      progressGradient.addColorStop(0, '#EE772F');
+      progressGradient.addColorStop((canvas.height * 0.7) / canvas.height, '#EB4926');
+      progressGradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff');
+      progressGradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff');
+      progressGradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#F6B094');
+      progressGradient.addColorStop(1, '#F6B094');
+    }
+    return {
+      waveColor: gradient,
+      progressColor: progressGradient,
+      autoplay: true,
+      height: 100,
+      barWidth: 2,
+      url: audioUrl,
+    };
+  }, [audioUrl]);
+  const wavesurfer = useWavesurfer(containerRef, optionsMemo);
+  // Initialize wavesurfer when the container mounts
+  // or any of the props change
+  useEffect(() => {
+    if (!wavesurfer) return;
+
+    const hover = hoverRef.current!;
+    const waveform = containerRef.current!;
+    waveform.addEventListener('pointermove', e => (hover.style.width = `${e.offsetX}px`));
+
+    const subscriptions = [
+      wavesurfer.on('decode', duration => {
+        setDuration(formatTime(duration));
+      }),
+      wavesurfer.on('timeupdate', currentTime => {
+        setTime(formatTime(currentTime));
+      }),
+      wavesurfer.once('interaction', () => {
+        wavesurfer.play();
+      }),
+    ];
+
+    return () => {
+      subscriptions.forEach(unsub => unsub());
+    };
+  }, [wavesurfer]);
 
   useEffect(() => {
     if (playingTrack !== null) {
@@ -49,8 +108,28 @@ function WavePlayer() {
   }, [playerStatus]);
 
   return (
-    <div className={styles.wavePlayerRoot}>
-      <div ref={containerRef} />
+    <div className='wave-player-root'>
+      <div
+        ref={containerRef}
+        className='wave-form-container'
+      >
+        <div className='time'>{time}</div>
+        <div className='duration'>{duration}</div>
+        <div
+          ref={hoverRef}
+          className='hover-wave'
+        ></div>
+        <div
+          className='overlay'
+          style={{
+            position: 'absolute',
+            height: '30px',
+            width: '100%',
+            bottom: '0',
+            backdropFilter: 'brightness(0.5)',
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
