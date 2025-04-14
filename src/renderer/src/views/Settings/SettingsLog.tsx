ee6bd44bf2eae413137e21a3797b0c9d2848c456
Jose Vega <josevega234@gmail.com>
2025-04-14 16:59:42 +0200

    reverse read

--- src/renderer/src/views/Settings/SettingsLog.tsx
+++ src/renderer/src/views/Settings/SettingsLog.tsx
@@ -1,24 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import styles from './Settings.module.css';
+import { ScrollAreaAutosize } from '@mantine/core';
 
 export default function SettingsLog() {
+  const viewPortRef = useRef<HTMLDivElement>(null);
   const { logger, app } = window.Main;
   const [logs, setLogs] = useState<string[]>([]);
 
   useEffect(() => {
     logger.info('SettingsLog mounted');
     app.getLogs().then(logsResponse => {
-      setLogs(logsResponse);
+      setLogs(logsResponse.reverse());
       logger.info(`logs size: ${logsResponse.length}`);
     });
   }, []);
 
   return (
     <div className={styles.settingsContainer}>
-      {logs.map((log, index) => (
-        <p key={index}>{log}</p>
-      ))}
+      <ScrollAreaAutosize
+        viewportRef={viewPortRef}
+        type='always'
+        scrollbars='y'
+        mah={900}
+      >
+        {logs.map((log, index) => (
+          <p key={index}>{log}</p>
+        ))}
+      </ScrollAreaAutosize>
     </div>
   );
 }
